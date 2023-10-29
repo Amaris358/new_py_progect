04ab142a5064ae8c714c6bb8ac8fc67072607c3e
Anastasia Puchinina <puchinina.anastasia58@gmail.com>
2023-10-30 01:55:38 +0700

    Create function get_difference

--- funcs.py
+++ funcs.py
@@ -1,3 +1,12 @@
 def get_summ(first_number, second_number):
     return first_number + second_number
 
+def get_difference(first_number, second_number):
+    return first_number - second_number
+
+
+
+
+
+
+
